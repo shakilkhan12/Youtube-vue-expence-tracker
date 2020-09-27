21214ad382fe0d1f22253626253accb6c8b31d92
Shakil Khan <shakilkhan621@gmail.com>
2020-09-27 10:02:51 +0500

    source code uploaded

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar @model-show="modelToggle" />
+  <Model
+    @model-toggle="modelToggle"
+    :status="modelStatus"
+    @store-expence="storeExpence"
+  />
+  <Expences :allExpences="expences" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar.vue";
+import Model from "./components/Model.vue";
+import Expences from "./components/Expences.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+    Model,
+    Expences,
+  },
+  data() {
+    return {
+      modelStatus: false,
+      expences: {
+        balance: 0,
+        income: 0,
+        expence: 0,
+        history: [],
+      },
+    };
+  },
+  methods: {
+    modelToggle() {
+      this.modelStatus = !this.modelStatus;
+    },
+    storeExpence(payload) {
+      const number = parseInt(payload.number);
+      if (number > 1) {
+        this.expences = {
+          ...this.expences,
+          income: this.expences.income + number,
+          balance: this.expences.balance + number,
+          history: [{ title: payload.title, number }, ...this.expences.history],
+        };
+      } else if (number < 1) {
+        this.expences = {
+          ...this.expences,
+          expence: this.expences.expence + number,
+          balance: this.expences.balance + number,
+          history: [{ title: payload.title, number }, ...this.expences.history],
+        };
+      }
+      this.modelStatus = false;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background: #fafafa;
+  font-family: sans-serif;
 }
 </style>
